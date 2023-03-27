7244e91ac4bcc604a0a802889814797a58c47b9a
tlldavid <tlrdavid@gmail.com>
2023-03-27 11:18:00 -0700

    Added new logic that shuffles a new deck when deck card count reaches below a certain threshold.

--- src/Dealer.java
+++ src/Dealer.java
@@ -4,5 +4,21 @@ public class Dealer extends Hand {
         super();
     }
 
+    // Method display dealer's face-up card. Uses timer to slow card presentation.
+    public void showUpCard() {
+        System.out.println("DEALER'S CARDS:");
+        for (Card hand : getHand()) {
+            Gameplay.delay(700);
+            System.out.println(hand);
+            Gameplay.delay(700);
+            System.out.println("(X) of XXXXXX");
+            Gameplay.delay(700);
+            System.out.println("Card value: " + hand.getRank().getValue());
+            Gameplay.delay(700);
+            break;
+        }
+        System.out.println();
+    }
+
 
 }
